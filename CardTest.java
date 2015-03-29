e0a02eb95b036b2a1b7f5b877d414d76e58275ee
Christopher Young <cwyoung11@comcast.net>
2015-03-29 18:01:12 -0400

    current card test
    updated test functionality

--- CardTest.java
+++ CardTest.java
@@ -6,25 +6,34 @@ import java.util.*;
 
 public class CardTest{
 	public static void main(String[] args){
-
 		int numberOfPlayers = 2;//get from gui setup IRL
 
 		Deck deck = new Deck();
 		deck.shuffle();
 		
-		deal(deck, numberOfPlayers);
+		Hand h1 = new Hand();
+		Hand h2 = new Hand();
 
-		Player p1 = new Player();
-		Player p2 = new Player();
 
-	}
-
-	public void deal(Deck deck, int numberOfPlayers){
-		for (int i=0; i<numberOfPlayers; i++){
-			for (int j=0; j<4; j++){
-				
-			}
+		for (int j=0; j<4; j++){
+			Card c = deck.getTopCard();
+			h1.add(c);
 		}
+
+		for (int i=0; i<4; i++){
+			Card c = deck.getTopCard();
+			h2.add(c);
+		}
+
+		Player p1 = new Player(h1, true);
+		Player p2 = new Player(h2, true);
+
+		System.out.println("Player 1's hand:");
+		p1.printHand();
+
+		System.out.println("Player 2's hand:");
+		p2.printHand();
 	}
+
 
 }
