8bc88f01aa6894045b95e5fea2c4734571770f08
Christopher Young <cwyoung11@comcast.net>
2015-03-29 18:15:17 -0400

    pile revision
    pile pile pile pile pile

--- Pile.java
+++ Pile.java
@@ -7,39 +7,26 @@ import java.util.*;
 public class Pile{
 
 	private String pileType;
-	private ArrayList <Card> pile;
+	private ArrayList <Card> discardPile;
 	private Deck deck;
 	private String deckImg;
 	
 	//Constructor
-	public DrawPile(){
-		pile = new ArrayList<Card>();
-
-		if(pileType=="draw"){
-			deckImg = "cardBack.jpg";
-		}else{
-			Card c = getTop();
-			c.getValue();
-		}
-
-
+	public Pile(){
+		discardPile = new ArrayList<Card>();
 	}
 	
-	public void add(Card card){
-		pile.add(card);
+	public void discardAdd(Card card){
+		discardPile.add(card);
 	}//add
 	
-	public Card getTopDraw(){
-		Card c = deck.get(0);
-		deck.remove(0);
+	public Card getTopDrawPile(){
+		Card c = deck.getTopCard();
 		return c;
 	}
 
-
-
-	public void clear(){
+	public void clear(ArrayList<Card> pile){
 		pile.clear();
-	}//clear
-	
+	}//clear deck or discard
 	
 }//Hand
