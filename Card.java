5daa1780053e23fecf8a470ad96d3def06ad560a
CS205FinalProject <hutchinson.h.christopher@uvm.edu>
2015-04-21 14:43:24 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,6 +6,8 @@
 public class Card{
 	private int value;//0-9, 10 for Draw2, 11 for Peek, 12 for Swap
 	private String pictureName;
+	private boolean known = false;
+   	private int location =0;
 
 	//Constructor
 	public Card(int v){
@@ -15,5 +17,38 @@ public class Card{
 	public int getValue(){
 		return value;
 	}
+	   public boolean isKnown()
+   {
+      return known;
+      
+   }
+   public void setKnown(boolean x)
+   {
+      if (x == true)
+      {
+         known = true;
+      }
+      else if (x == false)
+      {
+         known = false;
+      }
+      else
+      {
+         known = known;
+      }
+      
+      
+      
+   }
+   public void setLocation(int i)
+   {
+      location = i;
+   }
+   
+   public int getLocation()
+   {
+      return location;
+   
+   }
 
 }
